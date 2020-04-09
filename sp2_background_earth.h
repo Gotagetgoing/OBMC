3dbbb0b7b4ba609ce2ff8c31796b4a28e802290b
Gotagetgoing <38861533+Gotagetgoing@users.noreply.github.com>
2020-04-10 01:22:56 +0200

    Create sp2_background_earth.h
    Code added for the sp2_background_earth.h file.

---
+++ sp2_background_earth.h
@@ -0,0 +1,9 @@
+	  // Cloud shadow GFX objects
+	  // OBM
+	  CGFX_Model_Itf*                     m_pCloud2Model;
+	  GFX::GMesh*                         m_pCloud2Mesh;
+	  CGFX_Transform_SRT_Itf*             m_pCloud2Transform;
+    GFX::GMaterial*                     m_pCloud2Material;
+    CGFX_Texture_Itf*                   m_pCloud2Texture;
+    
+    
